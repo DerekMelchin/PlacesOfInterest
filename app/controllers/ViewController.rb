45e5913f1afdc59b23f60f810fbfdf3d7aac3424
Derek Melchin <derek@wndx.com>
2018-06-01 18:30:09 -0600

    Squash various bugs and reorganize code

--- app/controllers/ViewController.rb
+++ app/controllers/ViewController.rb
@@ -8,7 +8,7 @@ class ViewController < UIViewController
   attr_accessor :location_manager, :region_radius, :started_loading_POIs,
                 :places, :camera_button, :scene_view, :map_message_box,
                 :start_button, :exit_button, :curr_location, :destination,
-                :target_pos, :distance, :destination_altitude
+                :target_pos, :distance, :destination_altitude, :map_center
 
   def init
     @location_manager = CLLocationManager.alloc.init
@@ -22,7 +22,7 @@ class ViewController < UIViewController
     super
     display_map
     @location_manager.delegate = self
-    @location_manager.desiredAccuracy = 1000 # kCLLocationAccuracyNearestTenMeters
+    @location_manager.desiredAccuracy = 1000 #kCLLocationAccuracyNearestTenMeters
     @location_manager.requestWhenInUseAuthorization
     if @location_manager.headingAvailable
       @location_manager.startUpdatingHeading
@@ -55,78 +55,59 @@ class ViewController < UIViewController
     @scene_view.session.delegate = self
     self.view = @scene_view
     add_cones
+    add_message_box('Exit', 50)
+  end
 
+  def add_message_box(button_str, button_width)
     height = 70
-    ar_message_box = make_message_box(height)
     left_padding = 20
     vert_padding = 5
-
-    exit_width   = 50
-    exit_frame   = [[UIScreen.mainScreen.bounds.size.width - exit_width, 0],
-                    [exit_width, height]]
-    @exit_button = UIView.alloc.initWithFrame(exit_frame)
-    exit = UILabel.new
-    exit.font = UIFont.systemFontOfSize(18)
-    exit.text = 'Exit'
-    exit.textColor = UIColor.alloc.initWithRed(1, green: 1, blue: 1, alpha: 1.0)
-    exit.frame = [[0, 0], [exit_width, height]]
-    @exit_button.addSubview(exit)
-    ar_message_box.addSubview(@exit_button)
-
+    message_box_frame = [[0, UIScreen.mainScreen.bounds.size.height - height],
+                         [UIScreen.mainScreen.bounds.size.width, height]]
+    @message_box = UIView.alloc.initWithFrame(message_box_frame)
+    @message_box.backgroundColor = UIColor.alloc.initWithRed(0, green: 0.8, blue: 0.8, alpha: 0.9)
+    button_frame   = [[UIScreen.mainScreen.bounds.size.width - button_width, 0],
+                    [button_width, height]]
+    button_label = UILabel.new
+    button_label.font = UIFont.systemFontOfSize(18)
+    button_label.text = button_str
+    button_label.textColor = UIColor.alloc.initWithRed(1, green: 1, blue: 1, alpha: 1.0)
+    button_label.frame = [[0, 0], [button_width, height]]
+    if button_str == 'Exit'
+      @exit_button = UIView.alloc.initWithFrame(button_frame)
+      @exit_button.addSubview(button_label)
+      @message_box.addSubview(@exit_button)
+    else
+      @start_button = UIView.alloc.initWithFrame(button_frame)
+      @start_button.addSubview(button_label)
+      @message_box.addSubview(@start_button)
+    end
     name = UILabel.new
     name.font = UIFont.systemFontOfSize(16)
     name.text = "#{@destination.title}"
     name.textColor = UIColor.alloc.initWithRed(0, green: 0, blue: 0, alpha: 1)
-    name.frame = [[left_padding, vert_padding], [UIScreen.mainScreen.bounds.size.width - 2 * left_padding - exit_width, height / 2]]
-    ar_message_box.addSubview(name)
-
+    name.frame = [[left_padding, vert_padding],
+                  [UIScreen.mainScreen.bounds.size.width - 2 * left_padding - button_width, height / 2]]
+    @message_box.addSubview(name)
     @distance = UILabel.new
     @distance.font = UIFont.systemFontOfSize(18)
     @distance.text = "#{@curr_location.distanceFromLocation(@destination.location).round}m away"
     @distance.textColor = UIColor.alloc.initWithRed(0, green: 0, blue: 0, alpha: 0.5)
-    @distance.frame = [[left_padding, height / 2 - vert_padding], [UIScreen.mainScreen.bounds.size.width - 2 *  left_padding - exit_width, height / 2]]
-    ar_message_box.addSubview(@distance)
-
-    view.addSubview(ar_message_box)
-  end
-
-  def locationManager(manager, didUpdateLocations: locations)
-    if locations.count > 0
-      location = @curr_location = locations.last
-      puts "Accuracy: #{location.horizontalAccuracy}"
-      if location.horizontalAccuracy < 100
-        @location_manager.stopUpdatingLocation
-        span = MKCoordinateSpanMake(0.014, 0.014)
-        region = MKCoordinateRegionMake(location.coordinate, span)
-        view.setRegion(region, true)
-        unless @started_loading_POIs
-          @started_loading_POIs = true
-          loader = PlacesLoader.alloc.init
-          loader.load_POIs(self, location, 1000)
-        end
-      end
-    end
-  end
-
-  def touchesEnded(touches, withEvent: event)
-    display_AR if event.touchesForView(@start_button)
-    if event.touchesForView(@exit_button)
-      @scene_view.session.pause
-      display_map
-    end
+    @distance.frame = [[left_padding, height / 2 - vert_padding],
+                       [UIScreen.mainScreen.bounds.size.width - 2 * left_padding - button_width, height / 2]]
+    @message_box.addSubview(@distance)
+    view.addSubview(@message_box)
   end
 
   def add_cones
     scene = SCNScene.scene
-
     guide_geometry = SCNPyramid.pyramidWithWidth(0.1, height: 0.2, length: 0.1)
     guide_material = SCNMaterial.material
-    guide_material.diffuse.contents = NSColor.colorWithRed(0, green: 0.8, blue: 0.8, alpha: 0.9) # (0, green: 1, blue: 1, alpha: 0.8)
+    guide_material.diffuse.contents = NSColor.colorWithRed(0, green: 0.8, blue: 0.8, alpha: 0.9)
     guide_material.doubleSided = true
     guide_geometry.materials = [guide_material]
     guide = SCNNode.nodeWithGeometry(guide_geometry)
     guide.position = SCNVector3Make(0, 0.3, -1)
-
     target_geometry = SCNPyramid.pyramidWithWidth(2, height: 6, length: 2)
     target_material = SCNMaterial.material
     target_material.diffuse.contents = NSColor.colorWithRed(0, green: 1, blue: 0, alpha: 0.8)
@@ -134,11 +115,9 @@ class ViewController < UIViewController
     target_geometry.materials = [target_material]
     target = SCNNode.nodeWithGeometry(target_geometry)
     target.position = @target_pos = get_target_vec_location
-
     constraint = SCNLookAtConstraint.lookAtConstraintWithTarget(target)
     constraint.localFront = SCNVector3Make(0, 0.2, 0)
     guide.constraints = [constraint]
-
     @scene_view.pointOfView.addChildNode(guide)
     scene.rootNode.addChildNode(target)
     @scene_view.scene = scene
@@ -154,8 +133,6 @@ class ViewController < UIViewController
     meters_per_deg_lon = 111412.84 * Math.cos(radian_lat) - 93.5 * Math.cos(3 * radian_lat)
     x = (dest_lon - curr_lon) * meters_per_deg_lon
     z = (curr_lat - dest_lat) * meters_per_deg_lat
-
-    dest_alt = CLLocation.alloc.initWithLatitude(dest_lat, longitude: dest_lon)
 
     api_url = 'https://maps.googleapis.com/maps/api/elevation/'
     api_key = 'AIzaSyB8MZxrd9TRDvGBrAWJnFEtbQtrzgT2h7I'
@@ -174,7 +151,6 @@ class ViewController < UIViewController
         if response_object.class == NilClass # An error occurred with previous line
           error_handler(nil, error_ptr[0])
         elsif response_object.class != Hash
-          puts 'return'
           return
         else
           error_handler(response_object, nil)
@@ -183,11 +159,9 @@ class ViewController < UIViewController
     end
     data_task = session.dataTaskWithURL(url, completionHandler: completion_handler)
     data_task.resume
-
     while @destination_altitude.nil?
     end
     y = @destination_altitude - @curr_location.altitude
-
     SCNVector3Make(x, y, z)
   end
 
@@ -199,69 +173,83 @@ class ViewController < UIViewController
     end
   end
 
-  def make_message_box(height)
-    frame = [[0, UIScreen.mainScreen.bounds.size.height - height],
-             [UIScreen.mainScreen.bounds.size.width, height]]
-    message_box = UIView.alloc.initWithFrame(frame)
-    message_box.backgroundColor = UIColor.alloc.initWithRed(0, green: 0.8, blue: 0.8, alpha: 0.9)
-    message_box
+  # Called when the user moves locations
+  def locationManager(manager, didUpdateLocations: locations)
+    if locations.count > 0
+      location = @curr_location = locations.last
+      if location.horizontalAccuracy < 100
+        if @map_center.nil? or @map_center.distanceFromLocation(location) > 100
+          @map_center = location
+          span = MKCoordinateSpanMake(0.014, 0.014)
+          region = MKCoordinateRegionMake(location.coordinate, span)
+          view.setRegion(region, true)
+          @started_loading_POIs = false
+        end
+        unless @started_loading_POIs
+          @started_loading_POIs = true
+          loader = PlacesLoader.alloc.init
+          loader.load_POIs(self, location, 1000)
+        end
+      end
+      unless @distance.nil?
+        @distance.text = "~#{@curr_location.distanceFromLocation(@destination.location).round}m away"
+      end
+    end
+  end
+
+  # Called when the user touches the screen
+  def touchesEnded(touches, withEvent: event)
+    if event.touchesForView(@start_button)
+      @location_manager.stopUpdatingLocation
+      @map_center = nil
+      display_AR
+    end
+    if event.touchesForView(@exit_button)
+      @scene_view.session.pause
+      display_map
+    end
   end
 
   # Called when a map annotation is selected
   def mapView(mapView, didSelectAnnotationView: view)
     return if view.class.to_s == 'NSKVONotifying_MKModernUserLocationView'
-
+    @destination = nil
     @places.each do |place|
       if place.longitude == view.coordinate.longitude && place.latitude == view.coordinate.latitude
         @destination = place
         break
       end
     end
-
-    @map_message_box.removeFromSuperview unless @map_message_box.nil?
-
-    height = 70
-    left_padding = 20
-    vert_padding = 5
-    @map_message_box = make_message_box(height)
-
-    start_width   = 60
-    start_frame   = [[UIScreen.mainScreen.bounds.size.width - start_width, 0],
-                     [start_width, height]]
-    @start_button = UIView.alloc.initWithFrame(start_frame)
-    start = UILabel.new
-    start.font = UIFont.systemFontOfSize(18)
-    start.text = 'Start'
-    start.textColor = UIColor.alloc.initWithRed(1, green: 1, blue: 1, alpha: 1.0)
-    start.frame = [[0, 0], [start_width, height]]
-    @start_button.addSubview(start)
-    @map_message_box.addSubview(@start_button)
-
-    name = UILabel.new
-    name.font = UIFont.systemFontOfSize(16)
-    name.text = "#{@destination.title}"
-    name.textColor = UIColor.alloc.initWithRed(0, green: 0, blue: 0, alpha: 1)
-    name.frame = [[left_padding, vert_padding], [UIScreen.mainScreen.bounds.size.width - 2 * left_padding - start_width, height / 2]]
-    @map_message_box.addSubview(name)
-
-    distance = UILabel.new
-    distance.font = UIFont.systemFontOfSize(18)
-    distance.text = "#{@curr_location.distanceFromLocation(@destination.location).round}m away"
-    distance.textColor = UIColor.alloc.initWithRed(0, green: 0, blue: 0, alpha: 0.5)
-    distance.frame = [[left_padding, height / 2 - vert_padding], [UIScreen.mainScreen.bounds.size.width - 2 *  left_padding - start_width, height / 2]]
-    @map_message_box.addSubview(distance)
-
-    self.view.addSubview(@map_message_box)
+    if @destination.nil?
+      UIAlertView.alloc.initWithTitle('Out of Range',
+                                      message: 'The place of interest needs to be within 1km to enable AR.',
+                                      delegate: nil,
+                                      cancelButtonTitle: 'Ok',
+                                      otherButtonTitles: nil).show
+      return
+    end
+    add_message_box('Start', 60)
   end
 
   # Called when a map annotation is deselected
   def mapView(mapView, didDeselectAnnotationView: view)
-    @map_message_box.removeFromSuperview unless @map_message_box.nil?
+    @message_box.removeFromSuperview unless @message_box.nil?
   end
 
   # Called with every AR frame update
   def session(session, didUpdateFrame: frame)
     me = @scene_view.pointOfView.position
     @distance.text = "#{Math.sqrt((@target_pos.x - me.x)**2 + (@target_pos.z - me.z)**2).round}m away"
+  end
+
+  # Called when the AR session is interrupted
+  def sessionInterruptionEnded(session)
+    UIAlertView.alloc.initWithTitle('AR Session Interrupted',
+                                    message: 'Restart the AR experience to ensure target placement accuracy',
+                                    delegate: nil,
+                                    cancelButtonTitle: 'Ok',
+                                    otherButtonTitles: nil).show
+    @scene_view.session.pause
+    display_map
   end
 end
